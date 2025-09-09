47fc9cb4e5d7ba8a43b801df0b878e8c2ec8fd48
Aidan_Watkins <aidan.watkins@abingdon.org.uk>
2025-09-09 13:08:07 +0100

    fomat improvments

--- times tables.py
+++ times tables.py
@@ -1,6 +1,13 @@
 number=("Invalid")
-while not number.isdigit():
+valid= False
+while valid==False:
     number=input("which times table do you want to print out")
+    if not number.isdigit():
+        print("enter a number")
+    elif int(number)>=12 or int(number)<=1:
+        print("enter a number in between 1 and 12")
+    else:
+        valid=True
 number=int(number)
-for i in range(1,12):
-    print(str(i)+"X"+str(number)+"="+str(number*i))
+for i in range(1,13):
+    print(f'{str(i)} X {number} = {number*i}')
