9bd32cac4f39e9e29bc8b5b0fdf1089a963f42bc
Aidan_Watkins <aidan.watkins@abingdon.org.uk>
2025-11-03 19:23:39 +0000

    fix for peak+format improvments

--- linked list.py
+++ linked list.py
@@ -5,15 +5,12 @@ class Node:
 
     def set_next(self, node):
         self._next_node=node
-        pass
 
     def get_next(self):
         return self._next_node
-        pass
 
     def get_data(self):
         return self._data
-        pass
 
 class LinkedList:
     """
@@ -26,12 +23,11 @@ class LinkedList:
     def __len__(self):
         return self._size
         """ Allows the use of len(stack) to find the number of elements in the stack """
-        pass
 
     def push(self, data):
         self._head_node=Node(data,self._head_node)
         self._size+=1
-        pass
+
 
     def pop(self):
         if self._size==0:
@@ -44,8 +40,7 @@ class LinkedList:
         pass
 
     def peek(self):
-        return self._head_node
-        pass
+        return self._head_node.get_data()
     
     def is_empty(self):
         return self._size == 0
@@ -58,12 +53,10 @@ class LinkedList:
         string=""
         next=self._head_node
         while next!=None:
-            string+=str(next.get_data())
+            string+=str(next.get_data())+","
             next=next.get_next()
-
-
         """ Defines what should be displayed when the user prints a linked list object. """
-        return string
+        return string[0:-1]
 
 if __name__ == "__main__":
     my_stack = LinkedList()
@@ -72,6 +65,7 @@ if __name__ == "__main__":
     my_stack.push(2)
     my_stack.push(3)
     my_stack.append(4)
+    print(my_stack.peek())
 
     print(my_stack, len(my_stack))
 
