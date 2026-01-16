d0b872227dd151761374dc2b35cac97f1491c426
Aidan_Watkins <aidan.watkins@abingdon.org.uk>
2026-01-16 10:02:20 +0000

    first commit

---
+++ gui/convert.py
@@ -0,0 +1,44 @@
+import PyQt6.QtWidgets as qtw
+import PyQt6.QtCore as qtc
+# import PyQt6.QtCore as qtc
+# import PyQt6.QtGui as qtg
+
+
+# Subclass QMainWindow to customise your application's main window
+class MainWindow(qtw.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        layout = qtw.QGridLayout()
+
+        self.setWindowTitle("window")
+
+        widget = qtw.QWidget()
+
+        widget.setLayout(layout)
+
+        self.textbox=qtw.QLineEdit()
+
+        self.value=("?")
+
+        self.gobut=qtw.QPushButton("go")
+        self.gobut.clicked.connect(self.go_click)
+
+        self.setCentralWidget(widget)
+        self.label = qtw.QLabel()
+        layout.addWidget(self.label, 0, 0, 1, 3)
+        layout.addWidget(self.textbox, 1, 0)
+        layout.addWidget(self.gobut, 2, 0)
+
+    def go_click(self):
+        convert=self.textbox.text()
+        try:
+            self.value=str(int(convert)*3)
+        except:
+            self.value="?"
+        self.label.setText(self.value)
+        
+app = qtw.QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
