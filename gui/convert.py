1f0c47370e3005397bdcea89a5e25d3d2d389a41
Aidan_Watkins <aidan.watkins@abingdon.org.uk>
2026-01-16 10:16:24 +0000

    layout improvments

--- gui/convert.py
+++ gui/convert.py
@@ -19,23 +19,26 @@ class MainWindow(qtw.QMainWindow):
 
         self.textbox=qtw.QLineEdit()
 
-        self.value=("?")
-
         self.gobut=qtw.QPushButton("go")
         self.gobut.clicked.connect(self.go_click)
 
         self.setCentralWidget(widget)
         self.label = qtw.QLabel()
+        self.label2 = qtw.QLabel()
         layout.addWidget(self.label, 0, 0, 1, 3)
-        layout.addWidget(self.textbox, 1, 0)
-        layout.addWidget(self.gobut, 2, 0)
+        layout.addWidget(self.label2, 1, 0, 1, 1)
+        layout.addWidget(self.textbox, 1, 2)
+        layout.addWidget(self.gobut, 2, 0,1,3)
+        self.value="feet:  ?"
+        self.label.setText(self.value)
+        self.label2.setText("Metres:")
 
     def go_click(self):
         convert=self.textbox.text()
         try:
-            self.value="feet  "+str(int(convert)*3.28)
+            self.value="feet:  "+str(float(convert)*3.28)
         except:
-            self.value="feet  ?"
+            self.value="feet:  ?"
         self.label.setText(self.value)
         
 app = qtw.QApplication([])
